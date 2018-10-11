520e29db6afabb7073d0bc0d2c4adbca28a2ca4b
Gaivin <gaivin@outlook.com>
2018-10-11 05:19:58 +0000

    Update the DB ralated scripts

--- DjangoDemo/backup_db.sh
+++ DjangoDemo/backup_db.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+time=$(date "+%Y%m%d-%H%M%S")
+backup_file="../../dbbackup/DjangoDemo_db.json"
 echo "Start Database Backup..."
-python manage.py dumpdata --exclude auth.permission --exclude contenttypes > ../dbbackup/DjangoDemo_db.json
-echo "DB Backup Completed."
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes > ${backup_file}.${time} 
+echo "Backup Completed. Please check the backup file ${backup_file}.${time}"
