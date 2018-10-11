82d3e82b664e659386ca7d836f4fd89b3982e43c
wangg27 <Gaivin.Wang@emc.com>
2018-10-11 14:46:22 +0800

    Add the base template and using the python anywhere DB.

--- DjangoDemo/language/views.py
+++ DjangoDemo/language/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from language.chart import language_rank_chart, history_chart, world_cloud_chart
-
+from language.models import Language, Rating
 
 REMOTE_HOST = "https://pyecharts.github.io/assets/js"
 
@@ -30,4 +30,11 @@ def render_chart(chart_function):
         script_list=chart.get_js_dependencies()
     )
     html = loader.render_to_string(template_name="language_template.html", context=context)
+    return HttpResponse(html)
+
+
+def home_view(request):
+    languages = Language.objects.all()
+    context = dict(language_list=languages)
+    html = loader.render_to_string(template_name="home.html", context=context)
     return HttpResponse(html)
